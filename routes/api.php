43176ad345cfa79f7bb1fcfa62f5ec01d564ede2
Nicholas Ireland <nick@nicholasireland.ca>
2026-03-11 19:15:14 -0400

    Add: base url to API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+# /api
+Route::get('/', function () {
+    return json_encode(['hello' => 'world']);
+});
